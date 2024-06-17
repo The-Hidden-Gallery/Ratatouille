6ebf1e9d630cae13e291c0c275a457875403ff09
JavierRojo8 <202109117@alu.comillas.edu>
2024-06-17 20:19:05 +0200

    [FILES] - Added the dishes materials, as well as some code to access these materials from bproc, does not work yet because i don't know where it looks for the material

---
+++ bproc/data_sampler.py
@@ -0,0 +1,6 @@
+"""
+File to create a data sampler for the BProc project.
+It creates samples of positions, scales and rotations for the objects in the scene.
+It then attributes these samples a True or False label, depending on rules set by the user.
+This csv file can be imported in the blenderproc project to generate the images and attribute the labels accordingly
+"""
